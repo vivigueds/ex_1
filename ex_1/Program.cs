f312ee77bad42eeb47a321656eba47a3e001e0f7
ALUNO <ALUNO@LABFPC26>
2024-02-26 08:44:32 -0300

    Adicionar arquivos de projeto.

---
+++ ex_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 2 valores : ");
+            int a, b, s;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            s = a + b;
+
+            if ( s > 10)
+            {
+                s = s + 5;
+                Console.WriteLine(s);
+
+            }
+            else
+            {
+                s = s - 7;
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+        
+
+    }
+}
